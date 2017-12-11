fcc5ca6edf62bfd7f34381c08108dc6a01686985
Joshua <scott.joshua@gmail.com>
2017-12-11 10:48:35 -0700

    Inital commit

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 'use strict';
 
-const awsServerlessExpress = require('aws-serverless-express')
-const app = require('./app')
-const server = awsServerlessExpress.createServer(app)
+const AWS = require("aws-sdk");
+AWS.config.update({region: "us-west-2"});
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
+exports.handler = function(event, context, callback) {
+    docClient.get({
+        TableName: 'ShippingOptions',
+        Key: {CountryCode: event.CountryCode}
+    },  function(err, data) {
+        if(!data.Item){
+            console.log("No Shipping data found for" + event.CountryCode);
+        }
+        callback(err, data.Item);
+
+    });
+};
+
+
+
+
+
+
